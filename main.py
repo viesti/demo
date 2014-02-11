c774feb7f01a3515667a42bf9d983a2a8cc9bb57
Kimmo Koskinen <kimmo@tiuhti.net>
2014-02-11 13:38:28 +0200

    Change printout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# TODO: need a way to give name from argument
+
 def make_greeting(name):
     return "Hello %s!" % name
 
 if __name__ == "__main__":
-    print make_greeting("Demoer")
+    print make_greeting("There")
