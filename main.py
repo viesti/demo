db59fffcc03a8e3e04f6fc150fe3e86374423b9f
Kimmo Koskinen <kimmo@tiuhti.net>
2014-02-11 13:56:14 +0200

    Moar printing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# A great demo effect
 import sys
 
 def make_greeting(name):
     return "Hello %s!!" % name
 
+def make_another_greeting(name):
+    return "What's the clock %s!" % name
+
 if __name__ == "__main__":
     for name in sys.argv[1:]:
         print make_greeting(name)
+        print make_another_greeting(name)
